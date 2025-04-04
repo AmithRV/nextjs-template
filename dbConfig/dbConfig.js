4a15e9ab0561ccd2016d876d28dc4f972a37b2a0
Amith <amith@mojgenie.com>
2025-04-05 00:49:54 +0530

    commit

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 async function connect() {
   try {
     mongoose.connect(process.env.MONGO_URI);
+
     const connection = mongoose.connection;
 
     connection.on("connected", () => {
@@ -12,7 +13,7 @@ async function connect() {
     connection.on("error", (error) => {
       console.log("error while connecting to database.");
       console.log("-------------------------------------");
-      console.log(error);
+      console.log(error.errorResponse);
       console.log("-------------------------------------");
       process.exit();
     });

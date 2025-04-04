9c5826b8c52cef74feaa128d1dd42cb3a229dc4e
Amith <amith@mojgenie.com>
2025-04-04 22:00:00 +0530

    commit

---
+++ app/(pages)/auth/login/page.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+function page() {
+  return (
+    <div className="bg-gray-100 font-sans">
+      <div className="min-h-screen flex items-center justify-center px-4">
+        <div className="max-w-md w-full bg-white rounded-lg shadow-md p-8">
+          <div className="text-center mb-8">
+            <h1 className="text-2xl font-bold text-gray-800">Log In</h1>
+          </div>
+
+          <form>
+            <div className="mb-6">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-600 mb-2"
+              >
+                Username
+              </label>
+
+              <Input type="text" placeholder="username" className="px-4 py-3" />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600 mb-2"
+              >
+                Password
+              </label>
+
+              <Input
+                type="password"
+                placeholder="password"
+                className="px-4 py-3"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-3 rounded-md font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200"
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
---
+++ middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/auth/login" || path === "/auth/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/auth/login", request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/auth/login",
+    "/auth/signup",
+    "/dashboard",
+    "/stock-filter",
+    "/todo-list",
+  ],
+};

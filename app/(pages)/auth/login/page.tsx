e823686090b5323410f6f8a5719307007de6d1f1
Amith <amith@mojgenie.com>
2025-04-06 15:22:21 +0530

    commit

--- app/(pages)/auth/login/page.tsx
+++ app/(pages)/auth/login/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 import React from "react";
 import { Input } from "@/components/ui/input";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { cn } from "@/lib/utils";
+
+type Inputs = {
+  password: string;
+  userId: string;
+};
 
 function Login() {
-  const handleLogin = () => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="bg-gray-100 font-sans">
       <div className="min-h-screen flex items-center justify-center px-4">
@@ -12,22 +26,33 @@ function Login() {
             <h1 className="text-2xl font-bold text-gray-800">Log In</h1>
           </div>
 
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-6">
               <label
                 htmlFor="userId"
-                className="block text-sm font-medium text-gray-600 mb-2"
+                className="block text-sm font-bold text-gray-600 mb-2"
               >
                 UserId
               </label>
 
-              <Input type="text" placeholder="userId" className="px-4 py-3" />
+              <Input
+                type="text"
+                placeholder="userId"
+                className={cn("px-4 py-3", {
+                  "input-error": errors.userId,
+                })}
+                {...register("userId", { required: true })}
+              />
+
+              {errors.userId && (
+                <span className="form-error">userId required</span>
+              )}
             </div>
 
             <div className="mb-4">
               <label
                 htmlFor="password"
-                className="block text-sm font-medium text-gray-600 mb-2"
+                className="block text-sm font-bold text-gray-600 mb-2"
               >
                 Password
               </label>
@@ -35,17 +60,19 @@ function Login() {
               <Input
                 type="password"
                 placeholder="password"
-                className="px-4 py-3"
+                className={cn("px-4 py-3", {
+                  "input-error": errors.password,
+                })}
+                {...register("password", { required: true })}
               />
+              {errors.password && (
+                <span className="form-error">password required</span>
+              )}
             </div>
 
             <button
               type="submit"
               className="w-full bg-blue-600 text-white py-3 rounded-md font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200 cursor-pointer"
-              onClick={(e) => {
-                e.preventDefault();
-                handleLogin();
-              }}
             >
               Log In
             </button>
